637d5921be0f59dcf7b65abd3202df99ac1351e3
PIYUSH-JOSHI1 <134073167+PIYUSH-JOSHI1@users.noreply.github.com>
2024-06-20 19:58:56 +0530

    Update sort.py

--- sort.py
+++ sort.py
@@ -15,6 +15,13 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+
+
+import matplotlib
+matplotlib.use('agg')  # Use non-interactive backend
+
+# Other imports for sort.py
+
 from __future__ import print_function
 
 import os
